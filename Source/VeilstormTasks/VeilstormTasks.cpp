1d025921e47ba4584fa40cc58c47edf971224066
Mina Ashraf <mina.ashraf.sadk@gmail.com>
2024-04-19 19:06:38 +0200

    Initial Commit

---
+++ Source/VeilstormTasks/VeilstormTasks.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VeilstormTasks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VeilstormTasks, "VeilstormTasks" );
+ 
